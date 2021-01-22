b76b7e712ccc3d467fbd661d46af8f2cbc901b04
Ángel Barrera Sánchez <angel@sighup.io>
2021-01-22 17:03:36 +0100

    pin kubernetes provider as it contains breaking changes on v2

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     google      = ">= 3.44.0"
     google-beta = ">= 3.44.0"
-    kubernetes  = ">= 1.13.2"
+    kubernetes  = "= 1.13.3"
     null        = ">= 3.0.0"
     random      = ">= 3.0.0"
     external    = ">= 2.0.0"
