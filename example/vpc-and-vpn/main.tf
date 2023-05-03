a2e1793f2ae7703da66027b2a4ac63aba0a53589
Giuseppe Iannelli <94362884+g-iannelli@users.noreply.github.com>
2023-05-03 16:07:12 +0200

    chore(example,vpc-and-vpn): change required terraform version to >=1.3.0

--- example/vpc-and-vpn/main.tf
+++ example/vpc-and-vpn/main.tf
@@ -1,16 +1,16 @@
 module "vpc-and-vpn" {
-    source = "../../modules/vpc-and-vpn"
+  source = "../../modules/vpc-and-vpn"
 
-    name = "fury"
-    
-    network_cidr = "10.0.0.0/16"
-    public_subnetwork_cidrs = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    private_subnetwork_cidrs = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  name = "fury"
 
-    cluster_pod_subnetwork_cidr = "10.2.0.0/16"
-    cluster_service_subnetwork_cidr = "10.3.0.0/16"
-    cluster_subnetwork_cidr = "10.1.0.0/16"
+  network_cidr             = "10.0.0.0/16"
+  public_subnetwork_cidrs  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnetwork_cidrs = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-    vpn_subnetwork_cidr = "192.168.200.0/24"
-    vpn_ssh_users = ["github-user"]
+  cluster_pod_subnetwork_cidr     = "10.2.0.0/16"
+  cluster_service_subnetwork_cidr = "10.3.0.0/16"
+  cluster_subnetwork_cidr         = "10.1.0.0/16"
+
+  vpn_subnetwork_cidr = "192.168.200.0/24"
+  vpn_ssh_users       = ["github-user"]
 }
