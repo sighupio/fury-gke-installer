c40637b6048208a9df4785c3d6cb4f0d232eab70
Giuseppe Iannelli <94362884+g-iannelli@users.noreply.github.com>
2023-05-03 16:06:48 +0200

    chore(module,vpc-and-vpn): change required terraform version to >=1.3.0

--- modules/vpc-and-vpn/main.tf
+++ modules/vpc-and-vpn/main.tf
@@ -1,12 +1,12 @@
 data "google_client_config" "this" {}
 
 terraform {
-  required_version = "0.15.4"
+  required_version = ">=1.3.0"
   required_providers {
-    local    = "2.0.0"
-    null     = "3.0.0"
-    external = "2.0.0"
-    random   = "3.0.1"
-    google   = "3.55.0"
+    local    = "~>2.1.0"
+    null     = "~>3.1.1"
+    external = "~>2.1.1"
+    random   = "~>3.1.3"
+    google   = "~>3.63.0"
   }
 }
