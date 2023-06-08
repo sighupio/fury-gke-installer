766a343bc42f5e7e8d6e50ec300fde81edf49eca
Claudio Beatrice <claudi0.beatric3@gmail.com>
2023-06-08 17:32:58 +0200

    chore: refactor vpc module to split locals between modules+resources and outputs

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,3 +1,33 @@
+locals {
+  public_subnets = [for subnet in module.vpc.subnets :
+    {
+      subnet_name   = subnet.name
+      subnet_ip     = subnet.ip_cidr_range
+      subnet_region = subnet.region
+      secondary_ip_range_names = [for secondary_range in subnet.secondary_ip_range : secondary_range.range_name]
+    }
+    if contains(var.public_subnetwork_cidrs, subnet.ip_cidr_range)
+  ]
+
+  private_subnets = [for subnet in module.vpc.subnets :
+    {
+      subnet_name   = subnet.name
+      subnet_ip     = subnet.ip_cidr_range
+      subnet_region = subnet.region
+    }
+    if contains(var.private_subnetwork_cidrs, subnet.ip_cidr_range)
+  ]
+
+  cluster_private_subnet = [for subnet in module.vpc.subnets :
+    {
+      subnet_name   = subnet.name
+      subnet_ip     = subnet.ip_cidr_range
+      subnet_region = subnet.region
+    }
+    if contains([var.cluster_subnetwork_cidr], subnet.ip_cidr_range)
+  ]
+}
+
 output "network_name" {
   description = "The name of the network"
   value       = module.vpc.network_name
